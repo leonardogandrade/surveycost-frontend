f8b4bd552f8b07cd7afbce3799d7ba837d079722
Leonardo Gerheim de Andrade <leonardo.g.andrade@gmail.com>
2019-07-08 18:58:11 -0300

    gitignore

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
 
 const api = axios.create({
-    baseURL : 'http://localhost:3232/api',
+    baseURL : process.env.API_URL,
 });
 
 export default api;
